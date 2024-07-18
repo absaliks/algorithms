78c64621d977cac5b905754b5a65c847aa3e5adf
Shamil Absalikov <13360483+absaliks@users.noreply.github.com>
2024-07-18 20:56:48 +0200

    Solve "Longest Consecutive Sequence"

---
+++ src/test/java/absaliks/leetcode/LongestConsecutiveSequenceTest.java
@@ -0,0 +1,49 @@
+package absaliks.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/longest-consecutive-sequence
+// CPU 31ms | Beats 52.19%
+// RAM 61.22MB | Beats 64.78%
+public class LongestConsecutiveSequenceTest {
+
+    public int longestConsecutive(int... nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums)
+            set.add(num);
+
+        int result = 0;
+        for (int num : nums) {
+            int streak = 1;
+            for (int left = num - 1; set.contains(left); left--) {
+                set.remove(left);
+                streak++;
+            }
+            for (int right = num + 1; set.contains(right); right++) {
+                set.remove(right);
+                streak++;
+            }
+            result = Math.max(result, streak);
+        }
+        return result;
+    }
+
+    @Test
+    void test0() {
+        assertEquals(3, longestConsecutive(1, 3, 2));
+    }
+
+    @Test
+    void test1() {
+        assertEquals(4, longestConsecutive(100, 4, 200, 1, 3, 2));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(9, longestConsecutive(0, 3, 7, 2, 5, 8, 4, 6, 0, 1));
+    }
+}
